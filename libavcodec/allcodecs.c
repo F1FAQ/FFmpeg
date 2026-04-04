14c82ab64057fa9656fdad2ed62f4b47d819c6a8
f1faq <f1faq@free.fr>
2026-04-04 17:00:59 +0200

    Add ff_libvvdec_decoder to codec exports

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -831,6 +831,7 @@ extern const FFCodec ff_libvpx_vp8_decoder;
 extern FFCodec ff_libvpx_vp9_encoder;
 extern const FFCodec ff_libvpx_vp9_decoder;
 extern const FFCodec ff_libvvenc_encoder;
+extern const FFCodec ff_libvvdec_decoder;
 /* preferred over libwebp */
 extern const FFCodec ff_libwebp_anim_encoder;
 extern const FFCodec ff_libwebp_encoder;
